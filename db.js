8fea0cf2129df32a7c8d1b264b0d8d55c61c81c9
Zack Manfredi <manfredi.z@husky.neu.edu>
2020-07-25 09:52:44 -0400

    changes for heroku

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { MongoClient } = require('mongodb');
 let db;
 
 async function connectToDb() {
-  const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
+  const url = process.env.DB_URL || 'mongodb+srv://atlasZM:TH7945P7h2KxFe1T@cluster0.j7ncg.mongodb.net/issuetracker?retryWrites=true&w=majority';
   const client = new MongoClient(url, { useNewUrlParser: true });
   await client.connect();
   console.log('Connected to MongoDB at', url);
